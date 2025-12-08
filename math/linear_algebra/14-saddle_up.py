b6fe4a290d64d6a744c1e71d0a5ab52bacb6eb83
Luis May <a20214976@alumnos.uady.mx>
2025-12-07 22:19:04 -0600

    task 15

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Module that defines np_matmul, which
+performs matrix multiplication with NumPy."""
+
+
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """Return the matrix multiplication
+    of two NumPy arrays."""
+    return np.matmul(mat1, mat2)
