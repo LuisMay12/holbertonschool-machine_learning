d9cd678b983073f59c9570b27abebc3d34f2db0f
Luis May <a20214976@alumnos.uady.mx>
2025-12-07 21:54:05 -0600

    task 4

---
+++ math/linear_algebra/3-flip_me_over.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Module that defines matrix_transpose, which returns the transpose of a 2D matrix."""
+
+
+def matrix_transpose(matrix):
+    """Return the transpose of a 2D matrix."""
+    transposed = []
+    for c in range(len(matrix[0])):
+        row = []
+        for r in range(len(matrix)):
+            row.append(matrix[r][c])
+        transposed.append(row)
+    return transposed
