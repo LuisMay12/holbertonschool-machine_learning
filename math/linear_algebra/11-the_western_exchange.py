2a9b747a861fb8a06fba41b14af37f10d1947370
Luis May <a20214976@alumnos.uady.mx>
2025-12-07 22:13:21 -0600

    task 12

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module that defines np_transpose, which returns the
+transpose of a NumPy array."""
+
+def np_transpose(matrix):
+    """Return the transpose of a NumPy ndarray."""
+    return matrix.T
