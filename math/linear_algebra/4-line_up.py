b0ca4ff30c4e29bcfc8ce76c118b446391e3e438
Luis May <a20214976@alumnos.uady.mx>
2025-12-07 21:58:57 -0600

    task 5

---
+++ math/linear_algebra/4-line_up.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Module that defines add_arrays, which adds
+two arrays element-wise."""
+
+def add_arrays(arr1, arr2):
+    """Return a new list with element-wise sum of arr1 and arr2, or
+    None if shapes differ."""
+    if len(arr1) != len(arr2):
+        return None
+    return [arr1[i] + arr2[i] for i in range(len(arr1))]
