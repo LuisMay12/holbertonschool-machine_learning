81b9e508dcbcbf5f955327305b92d6499cbb5846
Luis May <a20214976@alumnos.uady.mx>
2025-12-07 21:59:57 -0600

    fix

--- math/linear_algebra/4-line_up.py
+++ math/linear_algebra/4-line_up.py
@@ -2,6 +2,7 @@
 """Module that defines add_arrays, which adds
 two arrays element-wise."""
 
+
 def add_arrays(arr1, arr2):
     """Return a new list with element-wise sum of arr1 and arr2, or
     None if shapes differ."""
