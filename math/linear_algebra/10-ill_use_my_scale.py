b7e877662078ec75594710cd175ad1a73c839b74
Luis May <a20214976@alumnos.uady.mx>
2025-12-07 22:11:23 -0600

    task 11

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Module that defines np_shape, which returns the
+shape of a NumPy array."""
+
+def np_shape(matrix):
+    """Return the shape of a NumPy ndarray as a
+    tuple of integers."""
+    return matrix.shape
