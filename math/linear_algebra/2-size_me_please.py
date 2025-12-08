b776fc3c4e6885f9d743452456f462212a0d9edb
Luis May <a20214976@alumnos.uady.mx>
2025-12-07 21:51:09 -0600

    fix

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+"""Module that defines matrix_shape, which calculates the shape of a matrix."""
+
 def matrix_shape(matrix):
     """Returns the shape of a matrix as a list of integers."""
     shape = []
