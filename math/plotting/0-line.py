8545563924a2b23c8a8d22871b413f6b45bd4c66
Luis May <a20214976@alumnos.uady.mx>
2025-12-15 21:47:01 -0600

    task 0

---
+++ math/plotting/0-line.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Plots y as a red line graph."""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def line():
+    """Plots a line graph of y = x^3 
+    for x in [0, 10]."""
+    y = np.arange(0, 11) ** 3
+    plt.figure(figsize=(6.4, 4.8))
+
+    plt.plot(y, 'r')
+    plt.xlim(0, 10)
+
+    plt.show()
