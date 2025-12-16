5e35ff9f96ecb455e55679066cb7233b261ae5c0
Luis May <72902161+LuisMay12@users.noreply.github.com>
2025-12-15 23:06:13 -0600

    Adjust histogram bins and range for student grades

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -12,9 +12,11 @@ def frequency():
     student_grades = np.random.normal(68, 15, 50)
     plt.figure(figsize=(6.4, 4.8))
 
-    plt.hist(student_grades, bins=10, edgecolor='black')
+    bins = range(0, 101, 10)
+    plt.hist(student_grades, bins=bins, range=(0, 100), edgecolor='black')
     plt.xlabel('Grades')
     plt.ylabel('Number of Students')
     plt.title('Project A')
+    plt.xlim(0, 100)
 
     plt.show()
