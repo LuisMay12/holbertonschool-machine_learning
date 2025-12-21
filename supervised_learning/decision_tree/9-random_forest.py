e6121865d5339b288927239fcceb3598c6909ce1
Luis May <a20214976@alumnos.uady.mx>
2025-12-21 13:43:56 -0600

    hopefully works

--- supervised_learning/decision_tree/9-random_forest.py
+++ supervised_learning/decision_tree/9-random_forest.py
@@ -48,7 +48,6 @@ class Random_Forest:
         rng = np.random.default_rng(self.seed)
 
         self.numpy_preds = []
-        self.bootstrap_indices = []
 
         depths = []
         n_nodes = []
@@ -58,7 +57,6 @@ class Random_Forest:
         for i in range(self.n_trees):
             # Bootstrap sampling
             indices = rng.integers(0, n_samples, n_samples)
-            self.bootstrap_indices.append(indices)
             X_bootstrap = explanatory[indices]
             y_bootstrap = target[indices]
 
@@ -67,7 +65,7 @@ class Random_Forest:
                 max_depth=self.max_depth,
                 min_pop=self.min_pop,
                 seed=self.seed + i,
-                split_criterion="Gini"
+                split_criterion="random"
             )
             T.fit(X_bootstrap, y_bootstrap)
 
@@ -81,26 +79,7 @@ class Random_Forest:
             self.numpy_preds.append(T.predict)
 
         if verbose == 1:
-            votes = [[] for _ in range(n_samples)]
-            for t, pred in enumerate(self.numpy_preds):
-                oob_mask = np.ones(n_samples, dtype=bool)
-                oob_mask[self.bootstrap_indices[t]] = False
-
-                oob_idx = np.where(oob_mask)[0]
-                if oob_idx.size == 0:
-                    continue
-
-                preds = pred(explanatory[oob_idx])
-                for i, p in zip(oob_idx, preds):
-                    votes[i].append(p)
-
-            final_preds = np.array([
-                max(set(v), key=v.count) if len(v) > 0 else -1
-                for v in votes
-            ])
-
-            valid = final_preds != -1
-            acc = np.mean(final_preds[valid] == target[valid])
+            acc = self.accuracy(self.explanatory, self.target)
             print(f"""  Training finished.
     - Mean depth                     : {np.mean(depths)}
     - Mean number of nodes           : {np.mean(n_nodes)}
