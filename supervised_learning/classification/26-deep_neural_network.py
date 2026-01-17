41a3a569b906d2cd2ff8b67efbe5aae8b2346aa0
Luis May <a20214976@alumnos.uady.mx>
2026-01-16 21:03:17 -0600

    docs

--- supervised_learning/classification/26-deep_neural_network.py
+++ supervised_learning/classification/26-deep_neural_network.py
@@ -49,6 +49,14 @@ class DeepNeuralNetwork:
         return self.__weights
 
     def forward_prop(self, X):
+        """Calculates the forward propagation of the neural network
+        
+        Args:
+            X: numpy.ndarray of shape (nx, m) that contains the input data
+        
+        Returns:
+            tuple: (output of the network, cache dictionary)
+        """
         self.__cache["A0"] = X
 
         for layer in range(1, self.__L + 1):
