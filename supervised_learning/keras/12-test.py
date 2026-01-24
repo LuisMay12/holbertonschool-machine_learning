732e424410b57ac6e5939cbc2b70c67f20d3d1c4
Luis May <a20214976@alumnos.uady.mx>
2026-01-24 16:11:18 -0600

    the obejct should be returned

--- supervised_learning/keras/12-test.py
+++ supervised_learning/keras/12-test.py
@@ -10,14 +10,13 @@ def test_model(network, data, labels, verbose=True):
     """
     Tests a neural network.
 
-    Args:
-        network: Keras model to test.
-        data: Input data to evaluate on.
-        labels: Correct one-hot labels for `data`.
-        verbose: If True, prints progress/results during evaluation.
+    network is the network model to test
+    data is the input data to test the model with
+    labels are the correct one-hot labels of data
+    verbose is a boolean that determines if output should be printed
 
-    Returns: the loss and accuracy of the model, respectively
+    Returns: the loss and accuracy of the model with the testing data,
+    respectively
     """
     v = 1 if verbose else 0
-    loss, acc = network.evaluate(data, labels, verbose=v)
-    return loss, acc
+    return network.evaluate(data, labels, verbose=v)
