ca46764a9c5dbcf4459e735b7e1fe8c8785d561d
Luis May <a20214976@alumnos.uady.mx>
2026-01-24 19:28:34 -0600

    maybe

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -1,19 +1,18 @@
 #!/usr/bin/env python3
-"""Plots a histogram of student grades."""
-
 import numpy as np
 import matplotlib.pyplot as plt
 
+np.random.seed(5)
+student_grades = np.random.normal(68, 15, 50)
 
-def frequency():
-    """Plots the frequency distribution of student grades."""
-    np.random.seed(5)
-    student_grades = np.random.normal(68, 15, 50)
-    plt.figure(figsize=(6.4, 4.8))
+# your code here
+bins = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
 
-    plt.hist(student_grades, bins=range(0, 110, 10), edgecolor='black')
-    plt.xlabel('Grades')
-    plt.ylabel('Number of Students')
-    plt.title('Project A')
-
-    plt.show()
+plt.xlabel('Grades')
+plt.ylim(0, 30)
+plt.xlim(0, 100)
+plt.ylabel('Number of Students')
+plt.title('Project A')
+plt.hist(student_grades, bins, edgecolor='black')
+plt.xticks(np.arange(0, 110, 10))
+plt.show()
