45933feb25833fb5a1bd631a57cb29d9de9ae059
Luis May <a20214976@alumnos.uady.mx>
2026-01-24 19:23:54 -0600

    trying again

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -6,17 +6,14 @@ import matplotlib.pyplot as plt
 
 
 def frequency():
-    """Plots the frequency distribution of
-    student grades."""
+    """Plots the frequency distribution of student grades."""
     np.random.seed(5)
     student_grades = np.random.normal(68, 15, 50)
     plt.figure(figsize=(6.4, 4.8))
 
-    bins = range(0, 101, 10)
-    plt.hist(student_grades, bins=bins, range=(0, 100), edgecolor='black')
+    plt.hist(student_grades, bins=10, edgecolor='black')
     plt.xlabel('Grades')
     plt.ylabel('Number of Students')
     plt.title('Project A')
-    plt.xlim(0, 100)
 
     plt.show()
