0dd407bf0da0b85cbe4db0e1ad886211cf476830
Luis May <a20214976@alumnos.uady.mx>
2026-01-24 19:43:40 -0600

    my bad?

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -17,6 +17,8 @@ def frequency():
     plt.title('Project A')
     plt.xlim(0, 100)
     plt.xticks(range(0, 110, 10))
-    plt.yticks(range(0, int(max(np.histogram(student_grades, bins=np.arange(0, 110, 10))[0])) + 5, 5))
+    # plt.yticks(range(0, int(max(np.histogram(student_grades, bins=np.arange(0, 110, 10))[0])) + 5, 5))
+    plt.ylim(0, 30)                 # fuerza eje Y hasta 30
+    plt.yticks(range(0, 31, 5))
 
     plt.show()
