49b1bff5619417d9034394129f76813aaa379628
Luis May <a20214976@alumnos.uady.mx>
2026-04-23 22:36:23 -0600

    i hope i dont have problems

--- unsupervised_learning/hyperparameter_tuning/0-gp.py
+++ unsupervised_learning/hyperparameter_tuning/0-gp.py
@@ -7,11 +7,11 @@ import numpy as np
 class GaussianProcess:
     """Represents a noiseless 1D Gaussian process."""
 
-    def __init__(self, X_init, Y_init, length=1, sigma_f=1):
+    def __init__(self, X_init, Y_init, l=1, sigma_f=1):
         """Initialize the Gaussian process."""
         self.X = X_init
         self.Y = Y_init
-        self.length = length
+        self.l = l
         self.sigma_f = sigma_f
         self.K = self.kernel(X_init, X_init)
 
@@ -20,4 +20,4 @@ class GaussianProcess:
         sqdist = np.sum(X1 ** 2, axis=1).reshape(-1, 1) + \
             np.sum(X2 ** 2, axis=1) - 2 * np.matmul(X1, X2.T)
 
-        return (self.sigma_f ** 2) * np.exp(-0.5 / (self.length ** 2) * sqdist)
+        return (self.sigma_f ** 2) * np.exp(-0.5 / (self.l ** 2) * sqdist)
