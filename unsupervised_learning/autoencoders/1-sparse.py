28f175ee466ebcb3dabed2b5ad85a59d835017c9
Luis May <a20214976@alumnos.uady.mx>
2026-04-26 19:39:37 -0600

    sparse autoencoder

---
+++ unsupervised_learning/autoencoders/1-sparse.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+"""Module that creates a sparse autoencoder."""
+
+import tensorflow.keras as keras
+
+
+def autoencoder(input_dims, hidden_layers, latent_dims, lambtha):
+    """Creates a sparse autoencoder.
+
+    Args:
+        input_dims: Integer containing the dimensions of the model input.
+        hidden_layers: List containing the number of nodes for each hidden
+            layer in the encoder.
+        latent_dims: Integer containing the dimensions of the latent space.
+        lambtha: Regularization parameter for L1 regularization on the
+            encoded output.
+
+    Returns:
+        encoder, decoder, auto:
+            encoder is the encoder model.
+            decoder is the decoder model.
+            auto is the sparse autoencoder model.
+    """
+    input_layer = keras.Input(shape=(input_dims,))
+    encoded = input_layer
+
+    for nodes in hidden_layers:
+        encoded = keras.layers.Dense(nodes, activation='relu')(encoded)
+
+    latent = keras.layers.Dense(
+        latent_dims,
+        activation='relu',
+        activity_regularizer=keras.regularizers.l1(lambtha)
+    )(encoded)
+
+    encoder = keras.Model(inputs=input_layer, outputs=latent)
+
+    latent_input = keras.Input(shape=(latent_dims,))
+    decoded = latent_input
+
+    for nodes in reversed(hidden_layers):
+        decoded = keras.layers.Dense(nodes, activation='relu')(decoded)
+
+    output_layer = keras.layers.Dense(
+        input_dims,
+        activation='sigmoid'
+    )(decoded)
+
+    decoder = keras.Model(inputs=latent_input, outputs=output_layer)
+
+    auto_input = keras.Input(shape=(input_dims,))
+    auto_output = decoder(encoder(auto_input))
+
+    auto = keras.Model(inputs=auto_input, outputs=auto_output)
+
+    auto.compile(optimizer='adam', loss='binary_crossentropy')
+
+    return encoder, decoder, auto
