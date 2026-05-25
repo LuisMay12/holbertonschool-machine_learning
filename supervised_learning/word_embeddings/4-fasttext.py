be399626ddedced1cad0925b8083f4242bb4dc9e
Luis May <a20214976@alumnos.uady.mx>
2026-05-24 22:51:21 -0600

    maybe

--- supervised_learning/word_embeddings/4-fasttext.py
+++ supervised_learning/word_embeddings/4-fasttext.py
@@ -32,4 +32,8 @@ def fasttext_model(sentences, vector_size=100, min_count=5, negative=5,
                                    seed=seed,
                                    workers=workers)
 
+    model.build_vocab(sentences)
+    model.train(sentences, total_examples=model.corpus_count,
+                epochs=model.epochs)
+
     return model
