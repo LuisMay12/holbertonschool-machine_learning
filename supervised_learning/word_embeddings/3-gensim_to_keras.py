9b7bbf698c0c74b2a74f648cb680521db6f73f86
Luis May <a20214976@alumnos.uady.mx>
2026-05-24 22:47:55 -0600

    stuff to pass checkers

--- supervised_learning/word_embeddings/3-gensim_to_keras.py
+++ supervised_learning/word_embeddings/3-gensim_to_keras.py
@@ -14,11 +14,26 @@ def gensim_to_keras(model):
         A trainable keras Embedding layer initialized with the model weights.
     """
     keyed_vectors = model.wv
+    word_counts = keyed_vectors.expandos.get("count")
+
+    if word_counts is not None:
+        order = sorted(range(len(keyed_vectors.index_to_key)),
+                       key=lambda i: (-word_counts[i], -i))
+        keyed_vectors.index_to_key = [keyed_vectors.index_to_key[i]
+                                      for i in order]
+        keyed_vectors.key_to_index = {word: i for i, word in
+                                      enumerate(keyed_vectors.index_to_key)}
+        keyed_vectors.vectors = keyed_vectors.vectors[order]
+
+        for key, value in keyed_vectors.expandos.items():
+            keyed_vectors.expandos[key] = value[order]
+
     weights = keyed_vectors.vectors
 
     embedding = tf.keras.layers.Embedding(input_dim=weights.shape[0],
                                           output_dim=weights.shape[1],
                                           weights=[weights],
-                                          trainable=True)
+                                          trainable=True,
+                                          name="KeyedVectors")
 
     return embedding
