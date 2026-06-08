d9925e6432b29704c2c207be79d0785fffdee91b
Luis May <a20214976@alumnos.uady.mx>
2026-06-07 22:32:24 -0600

    encode

---
+++ supervised_learning/transformer_apps/1-dataset.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+"""Dataset module for machine translation transformer applications."""
+
+import transformers
+import tensorflow_datasets as tfds
+
+
+class Dataset:
+    """Loads and prepares a Portuguese to English translation dataset."""
+
+    def __init__(self):
+        """Initialize the training/validation data and tokenizers."""
+        self.data_train = tfds.load(
+            'ted_hrlr_translate/pt_to_en',
+            split='train',
+            as_supervised=True,
+            try_gcs=True
+        )
+        self.data_valid = tfds.load(
+            'ted_hrlr_translate/pt_to_en',
+            split='validation',
+            as_supervised=True,
+            try_gcs=True
+        )
+        self.tokenizer_pt, self.tokenizer_en = self.tokenize_dataset(
+            self.data_train
+        )
+
+    def tokenize_dataset(self, data):
+        """Create sub-word tokenizers trained from the given dataset."""
+        tokenizer_pt = transformers.AutoTokenizer.from_pretrained(
+            'neuralmind/bert-base-portuguese-cased'
+        )
+        tokenizer_en = transformers.AutoTokenizer.from_pretrained(
+            'bert-base-uncased'
+        )
+
+        def get_training_corpus(index):
+            """Generate batches of sentences for tokenizer training."""
+            for pt, en in data.batch(1000):
+                if index == 0:
+                    yield [sentence.decode('utf-8') for sentence in pt.numpy()]
+                else:
+                    yield [sentence.decode('utf-8') for sentence in en.numpy()]
+
+        tokenizer_pt = tokenizer_pt.train_new_from_iterator(
+            get_training_corpus(0),
+            vocab_size=2 ** 13
+        )
+        tokenizer_en = tokenizer_en.train_new_from_iterator(
+            get_training_corpus(1),
+            vocab_size=2 ** 13
+        )
+
+        return tokenizer_pt, tokenizer_en
+
+    def encode(self, pt, en):
+        """Encode Portuguese and English sentences into token ids."""
+        pt_tokens = self.tokenizer_pt.encode(
+            pt.numpy().decode('utf-8'),
+            add_special_tokens=False
+        )
+        en_tokens = self.tokenizer_en.encode(
+            en.numpy().decode('utf-8'),
+            add_special_tokens=False
+        )
+
+        pt_vocab_size = self.tokenizer_pt.vocab_size
+        en_vocab_size = self.tokenizer_en.vocab_size
+
+        pt_tokens = [pt_vocab_size] + pt_tokens + [pt_vocab_size + 1]
+        en_tokens = [en_vocab_size] + en_tokens + [en_vocab_size + 1]
+
+        return pt_tokens, en_tokens
