4d1c6b2d0439cfff861cc3cd8c2941d3f301ee2a
Luis May <a20214976@alumnos.uady.mx>
2026-06-07 23:11:34 -0600

    acquisition

---
+++ unsupervised_learning/hyperparameter_tuning/4-bayes_opt.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+"""Bayesian optimization acquisition module."""
+
+import numpy as np
+from scipy.stats import norm
+
+GP = __import__('2-gp').GaussianProcess
+
+
+class BayesianOptimization:
+    """Performs Bayesian optimization on a noiseless 1D Gaussian process."""
+
+    def __init__(self, f, X_init, Y_init, bounds, ac_samples, l=1,
+                 sigma_f=1, xsi=0.01, minimize=True):
+        """Initialize the Bayesian optimization object."""
+        self.f = f
+        self.gp = GP(X_init, Y_init, l, sigma_f)
+        self.X_s = np.linspace(bounds[0], bounds[1], ac_samples).reshape(-1, 1)
+        self.xsi = xsi
+        self.minimize = minimize
+
+    def acquisition(self):
+        """Calculate the next best sample location."""
+        mu, sigma = self.gp.predict(self.X_s)
+        std = np.sqrt(sigma)
+
+        if self.minimize:
+            best = np.min(self.gp.Y)
+            improvement = best - mu - self.xsi
+        else:
+            best = np.max(self.gp.Y)
+            improvement = mu - best - self.xsi
+
+        Z = np.zeros_like(improvement)
+        np.divide(improvement, std, out=Z, where=std != 0)
+        EI = improvement * norm.cdf(Z) + std * norm.pdf(Z)
+        EI[std == 0] = 0
+
+        X_next = self.X_s[np.argmax(EI)]
+
+        return X_next, EI
